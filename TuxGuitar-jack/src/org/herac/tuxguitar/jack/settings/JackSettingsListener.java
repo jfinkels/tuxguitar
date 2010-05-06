f6fbd1cbf6e6d0d53e7a0c805ab628b44cb62898
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2010-05-06 00:27:15 -0400

    restructured project, for linux only

--- TuxGuitar-jack/src/org/herac/tuxguitar/jack/settings/JackSettingsListener.java
+++
@@ -1,9 +0,0 @@
-package org.herac.tuxguitar.jack.settings;
-
-import org.herac.tuxguitar.gui.system.config.TGConfigManager;
-
-public interface JackSettingsListener {
-	
-	public void loadSettings( TGConfigManager config );
-	
-}
--- TuxGuitar/src/org/herac/tuxguitar/player/base/MidiOutputPort.java
+++
@@ -1,7 +0,0 @@
-package org.herac.tuxguitar.player.base;
-
-public interface MidiOutputPort extends MidiDevice {
-	
-	public MidiReceiver getReceiver() throws MidiPlayerException;
-	
-}
