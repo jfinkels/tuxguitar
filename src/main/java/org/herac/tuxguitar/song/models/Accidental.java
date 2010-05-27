d5f1c41b13dfb6e8efc7e14e654765b78b019545
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2010-05-27 00:28:58 -0400

    added documentation

--- src/main/java/org/herac/tuxguitar/song/models/Accidental.java
+++ src/main/java/org/herac/tuxguitar/song/models/Accidental.java
@@ -1,11 +1,29 @@
 package org.herac.tuxguitar.song.models;
 
+/**
+ * An accidental, that is, either a flat, a natural, or a sharp.
+ * 
+ * @author Jeffrey Finkelstein
+ */
 public enum Accidental {
-  FLAT(3), SHARP(2), NATURAL(1);
-  
+  /** A flat, denoting one half step down from the current note. */
+  FLAT(3),
+  /** A natural, denoting the current note is to be played as is. */
+  NATURAL(1),
+  /** A sharp, denoting one half step up from the current note. */
+  SHARP(2);
+
+  /** The unique identifying number of this type of accidental. */
+  private final int id;
+
+  /**
+   * Instantiates this type of accidental with the specified unique identifying
+   * number.
+   * 
+   * @param id
+   *          The unique identifying number of this type of accidental.
+   */
   private Accidental(final int id) {
     this.id = id;
   }
-
-  private final int id;
 }
